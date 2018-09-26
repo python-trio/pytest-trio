ffbceceffc2a59d7283fcddc622fe4b2224dff9e
Nathaniel J. Smith <njs@pobox.com>
2018-09-26 01:35:22 -0700

    Hack to let us keep merging stuff while gh-66 is being resolved

--- pytest_trio/_tests/conftest.py
+++ pytest_trio/_tests/conftest.py
@@ -1,1 +1,10 @@
+# Temporary hack while waiting for an answer here:
+#    https://github.com/pytest-dev/pytest/issues/4039
+import pytest
+import warnings
+warnings.filterwarnings(
+    "default", category=pytest.RemovedInPytest4Warning,
+    message=".*non-top-level conftest.*",
+)
+
 pytest_plugins = ["pytester"]
